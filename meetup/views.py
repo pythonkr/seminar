e565c53e2537ba61eda07ea25c6836ccb86bba65
Noh Seho <iam@sehonoh.kr>
2017-09-11 20:26:54 +0900

    Route to template

--- meetup/views.py
+++ meetup/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def past(request):
+    return render(request, 'past.html')
+
+
+def past_list(request):
+    return render(request, 'pastlist.html')
+
+
+def profile(request):
+    return render(request, 'profile.html')
+
+
+def coc(request):
+    return render(request, 'coc.html')
+
+
+def speaker(request):
+    return render(request, 'speaker.html')
+
+
+def speaker_list(request):
+    return render(request, 'speakerlist.html')
+
+
+def schedule(request):
+    return render(request, 'schedule.html')
+
+
+def program(request):
+    return render(request, 'program.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def mailsent(request):
+    return render(request, 'mailsent.html')
