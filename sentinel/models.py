847f2d130ed225d4d90f2492ece253865390f207
Soyoun(Rose) Kim <makeajourney@gmail.com>
2017-09-24 12:50:59 +0900

    model test(not completed)

--- sentinel/models.py
+++ sentinel/models.py
@@ -19,21 +19,19 @@ class EmailToken(models.Model):
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None):
         if not email:
-            raise ValueError('Users must have an email address')
+            raise ValueError(_('Users must have an email address'))
 
         user = self.model(
             email=self.normalize_email(email),
         )
-
-        user.set_password(password)
         user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password):
         user = self.create_user(
             email,
-            password=password,
         )
+        user.set_password(password)
         user.is_admin = True
         user.save(using=self._db)
         return user
