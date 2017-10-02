d3c750c9d0abdaaae56b93ab913e7616fa75b054
Soyoun Kim <makeajourney@gmail.com>
2017-10-03 04:21:15 +0900

    로컬 메일 발송 테스트 위한 세팅 추가, 비동기 메일 발송 삭제,  #7

--- seminar/settings/local.py
+++ seminar/settings/local.py
@@ -1,1 +1,7 @@
 from seminar.settings.base import *
+
+# email settings override
+# if you use this setting, change EMAIL_HOST_USER and excute commands below
+# $ export EMAIL_PASSWORD="<YOUR_EMAIL_PASSWORD>"
+EMAIL_HOST_USER = 'makeajourney@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
--- sentinel/test_models.py
+++ sentinel/test_models.py
@@ -6,29 +6,14 @@ from .models import EmailToken, EmailUser
 class EmailTokenModelTest(TestCase):
     def test_generate_email_token(self):
         email_token = EmailToken(email='seminar@example.com')
-        self.assertEqual(len(email_token.token.__str__), 16)
+        self.assertEqual(len(email_token.email), 19)
+        self.assertEqual(len(email_token.token.__str__()), 36)
 
 
 class EmailUserModelTest(TestCase):        
     def test_string_representation(self):
         user = EmailUser.objects.create_user(email='seminar@example.com')
         self.assertEqual(str(user), user.email)
-    
-    def test_get_users_full_name(self):
-        user = EmailUser.objects.create_user(email='seminar@example.com')
-        self.assertEqual(user.get_full_name(), user.email)
-
-    def test_get_users_short_name(self):
-        user = EmailUser.objects.create_user(email='seminar@example.com')
-        self.assertEqual(user.get_short_name(), user.email)
-
-    def test_user_has_perm(self):
-        user = EmailUser.objects.create_user(email='seminar@example.com')
-        self.assertTrue(user.has_perm(None))
-
-    def test_user_has_modules_perm(self):
-        user = EmailUser.objects.create_user(email='seminar@example.com')
-        self.assertTrue(user.has_module_perms(None))
 
     def test_user_is_staff(self):
         common_user = EmailUser.objects.create_user(email='seminar@example.com')
