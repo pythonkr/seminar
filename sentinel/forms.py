dc9c9e3c317d4106aa76f639fad8470509af247d
Soyoun Kim <makeajourney@gmail.com>
2017-10-09 02:54:44 +0900

    로그인 form에서 email 입력상자에 auto focus 속성 추가

--- sentinel/forms.py
+++ sentinel/forms.py
@@ -3,6 +3,10 @@ from django import forms
 
 class EmailLoginForm(forms.Form):
     email = forms.EmailField()
-    email.widget.attrs.update({
+
+    def __init__(self, *args, **kwargs):
+        super(forms.Form, self).__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs.update({
             'class': 'input__wide',
+            'autofocus': 'autofocus',
         })
